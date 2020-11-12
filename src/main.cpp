842c1c8fa231f2bf609856a2f79969bec8f21824
Charlie <charliesewell@live.com>
2020-11-12 16:29:18 +0800

    Adding first window code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
+
+
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#define GLFW_INCLUDE_NONE
 #include <iostream>
 #include <glm/glm.hpp>
-#include <glfw/glfw3.h>
+void error_callback(int error, const char* description)
+{
+    std::cout << "Error: %s\n"<< std::endl;
+}
+
+
 int main() {
-    glm::vec4 test;
-	std::cout << "wow this kinda work!" << std::endl;
-	return 0;
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL())
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    while (!glfwWindowShouldClose(window))
+    {
+
+
+    }
+    glfwDestroyWindow(window);
+    return 0;
 }
