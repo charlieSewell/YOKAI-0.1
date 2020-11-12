c5303ed02231bcd5f4d38b135c906712ebbe480b
Charlie <charliesewell@live.com>
2020-11-12 14:00:58 +0800

    Working on OSX
    Getting code to compile on OSX using CLion

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <glm/glm.hpp>
+#include <glfw/glfw3.h>
 int main() {
     glm::vec4 test;
-	std::cout << "wow this really works!" << std::endl;
+	std::cout << "wow this kinda work!" << std::endl;
 	return 0;
 }
