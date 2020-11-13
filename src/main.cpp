5c25d1703fd64de507e02d848bdd5e60ec95c48e
Charlie <charliesewell@live.com>
2020-11-13 10:17:39 +0800

    window working# Please enter the commit message for your changes. Lines starting

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,13 @@ int main() {
     }
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
 
     }
     glfwDestroyWindow(window);
