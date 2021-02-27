bda643179b61229c42592da25b9655c51d1afae0
Charlie Sewell <charliesewell@Live.com>
2021-02-27 15:15:59 +0800

    Adding Renderer Class and changing model loader

---
+++ src/Renderer/Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Charlie Sewell on 25/02/2021.
+//
+
+#include "Renderer.hpp"
---
+++ src/Renderer/Renderer.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Charlie Sewell on 25/02/2021.
+//
+
+#ifndef ICT397_GAME_ENGINE_RENDERER_HPP
+#define ICT397_GAME_ENGINE_RENDERER_HPP
+
+
+
+class Renderer {
+
+};
+
+
+
+#endif //ICT397_GAME_ENGINE_RENDERER_HPP
