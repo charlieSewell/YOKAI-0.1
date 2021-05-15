651d58986d057d5e27b36d03ba2f2e098f1a992f
Charlie Sewell <charliesewell@live.com>
2021-05-15 15:37:20 +0800

    Fixed Physics

--- src/Test/ReactTerrainShape.cpp
+++ src/Test/ReactTerrainShape.cpp
@@ -14,16 +14,12 @@ ReactTerrainShape::~ReactTerrainShape()
 void ReactTerrainShape::CreateTerrainShape(reactphysics3d::PhysicsCommon &physicsCommon,reactphysics3d::PhysicsWorld *physicsWorld)
 {
     int terrainSize = TerrainFactory::getInstance().getTerrainSize();
-    //TODO: TERRAIN Y HARDCODED AND NEEDS TO CHANGE
-    rp3d::Vector3 position(terrainSize/2, 117, terrainSize/2);
+    rp3d::Vector3 position(terrainSize/2, 128, terrainSize/2);
     rp3d::Quaternion orientation = rp3d::Quaternion::identity();
     reactphysics3d::Transform transform = reactphysics3d::Transform(position, orientation);
     body = physicsWorld->createCollisionBody(transform);
     shapeID = body->getEntity().id;
     std::cout <<shapeID <<std::endl;
-    float mMaxHeight = -300;
-    float mMinHeight = 1000;
-
 
 
     for(int x=0; x<terrainSize; ++x)
@@ -31,11 +27,9 @@ void ReactTerrainShape::CreateTerrainShape(reactphysics3d::PhysicsCommon &physic
         for(int z=0; z<terrainSize; ++z)
         {
             heightFieldArray[x * terrainSize + z] = TerrainFactory::getInstance().heightAt(static_cast<float>(z),static_cast<float>(x));
-            if (heightFieldArray[x * terrainSize + z] > mMaxHeight) mMaxHeight =    heightFieldArray[x * terrainSize + z] ;
-            if (heightFieldArray[x * terrainSize + z] < mMinHeight) mMinHeight = heightFieldArray[x * terrainSize + z] ;
         }
     }
-    terrainCollider = physicsCommon.createHeightFieldShape(terrainSize, terrainSize, mMinHeight, mMaxHeight, heightFieldArray, rp3d::HeightFieldShape::HeightDataType::HEIGHT_FLOAT_TYPE);
+    terrainCollider = physicsCommon.createHeightFieldShape(terrainSize, terrainSize, 0, 255, heightFieldArray, rp3d::HeightFieldShape::HeightDataType::HEIGHT_FLOAT_TYPE);
     body->addCollider(terrainCollider, rp3d::Transform::identity());
 }
 void ReactTerrainShape::DeleteTerrainShape(reactphysics3d::PhysicsCommon &physicsCommon,reactphysics3d::PhysicsWorld *physicsWorld){
